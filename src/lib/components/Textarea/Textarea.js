7b5b42e7003052dddc7cb23c88cc5b40cefa020d
Theo Marinho <thmarinho@outlook.fr>
2023-04-22 21:11:49 +0200

    del: remove useless textarea props

--- src/lib/components/Textarea/Textarea.js
+++ src/lib/components/Textarea/Textarea.js
@@ -3,45 +3,35 @@ import React from 'react'
 import { classNames, getGradient } from '../../utils';
 
 const Textarea = ({
-	placeholder,
 	style,
 	type,
+	...args
 }) => {
 	return (
 		<div
 			className={
 				classNames(
-					"rounded-[1rem]",
+					"rounded-2xl",
 					"w-full",
 					"p-1",
 					getGradient(style)
 				)}
 		>
 			<textarea
-				placeholder={placeholder}
 				type={type}
-				className={
-					classNames(
-						"w-full",
-						"block align-bottom",
-						"rounded-[0.8rem]",
-						"text-md leading-tight",
-						"py-2 px-4",
-						"bg-white text-black",
-					)}>
-			</textarea>
+				className="w-full align-bottom rounded-xl text-md leading-tight py-2 px-4 bg-white text-black"
+				{...args}
+			/>
 		</div>
 
 	);
 };
 
 Textarea.propTypes = {
-	placeholder: PropTypes.string.isRequired,
 	style: PropTypes.oneOf(["primary", "secondary"]).isRequired,
 }
 
 Textarea.defaultProps = {
-	placeholder: 'Textarea',
 	style: 'primary',
 }
 
--- src/lib/components/Textarea/Textarea.stories.js
+++ src/lib/components/Textarea/Textarea.stories.js
@@ -12,10 +12,8 @@ export const Primary = Template.bind({})
 export const Secondary = Template.bind({})
 
 Primary.args = {
-	placeholder: 'Primary placeholder',
 	style: 'primary',
 }
 Secondary.args = {
-	placeholder: 'Secondary placeholder',
 	style: 'secondary',
 }
