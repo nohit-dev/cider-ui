bf9f563c44ea71c22d8a0360db9f23df619405c8
Theo Marinho <thmarinho@outlook.fr>
2023-04-22 20:49:08 +0200

    del: remove useless input props

--- src/lib/components/Input/Input.js
+++ src/lib/components/Input/Input.js
@@ -3,46 +3,33 @@ import React from 'react'
 import { classNames, getGradient } from '../../utils';
 
 const Input = ({
-	placeholder,
 	style,
-	type,
+	...args
 }) => {
 	return (
 		<div
 			className={
 				classNames(
 					"rounded-full",
-					"transition duration-150",
 					"p-1",
 					getGradient(style)
 				)}
 		>
 			<input
-				placeholder={placeholder}
-				type={type}
-				className={
-					classNames(
-						"w-full",
-						"rounded-full",
-						"text-lg",
-						"py-2 px-6",
-						"bg-white text-black",
-					)} />
+				className="w-full rounded-full text-lg py-2 px-6 bg-white text-black"
+				{...args}
+			/>
 		</div>
 
 	);
 };
 
 Input.propTypes = {
-	placeholder: PropTypes.string.isRequired,
 	style: PropTypes.oneOf(["primary", "secondary"]).isRequired,
-	type: PropTypes.oneOf(["text", "password", "email", "number", "tel"]).isRequired,
 }
 
 Input.defaultProps = {
-	placeholder: 'Input',
 	style: 'primary',
-	type: 'text',
 }
 
 export default Input
--- src/lib/components/Input/Input.stories.js
+++ src/lib/components/Input/Input.stories.js
@@ -10,38 +10,12 @@ const Template = (args) => <Input {...args} />
 
 export const Primary = Template.bind({})
 export const Secondary = Template.bind({})
-export const Password = Template.bind({})
-export const Email = Template.bind({})
-export const Number = Template.bind({})
-export const Tel = Template.bind({})
 
 Primary.args = {
 	placeholder: 'Primary placeholder',
 	style: 'primary',
-	type: 'text',
 }
 Secondary.args = {
 	placeholder: 'Secondary placeholder',
 	style: 'secondary',
-	type: 'text',
-}
-Password.args = {
-	placeholder: 'Password placeholder',
-	style: 'secondary',
-	type: 'password',
-}
-Email.args = {
-	placeholder: 'contact@cider-ui.com',
-	style: 'secondary',
-	type: 'email',
-}
-Number.args = {
-	placeholder: 'Number placeholder',
-	style: 'secondary',
-	type: 'number',
-}
-Tel.args = {
-	placeholder: 'Tel placeholder',
-	style: 'secondary',
-	type: 'tel',
 }
