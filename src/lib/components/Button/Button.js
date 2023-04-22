6df2be6b9a18c9a06974ee0839e94168134f3a57
Theo Marinho <thmarinho@outlook.fr>
2023-04-22 16:06:30 +0200

    fix: fix button size

--- src/lib/components/Button/Button.js
+++ src/lib/components/Button/Button.js
@@ -7,40 +7,47 @@ const Button = ({
 	filled,
 	style,
 	size,
-	onClick,
 	leftIcon,
 	rightIcon,
 	...args
 }) => {
+
+	const borderWidth = {
+		small: "p-1",
+		medium: "p-1.5",
+		large: "p-1.5"
+	}
+
+	const paddingSize = {
+		small: "py-1.5 px-3 text-xs",
+		medium: "py-2 px-5 text-md",
+		large: "py-2.5 px-7 text-lg"
+	}
+
 	return (
 		<button
-			onClick={onClick}
 			className={
 				classNames(
 					"rounded-full",
-					size === "small" && "scale-75 hover:scale-[.8]",
-					size === "medium" && "scale-100 hover:scale-105",
-					size === "large" && "scale-[1.2] hover:scale-[1.25]",
+					"scale-100 hover:scale-105",
 					"transition duration-150",
-					filled ? "" : "p-1.5",
+					!filled && borderWidth[size],
 					getGradient(style)
 				)}
-				{...args}
+			{...args}
 		>
 			<div className={
 				classNames(
 					"font-bold",
 					"w-full",
 					"rounded-full",
-					"text-lg",
-					"py-2 px-6",
+					"flex items-center",
+					paddingSize[size],
 					filled ? "bg-transparent text-white fill-white" : "bg-white text-black fill-black",
-					"flex",
-					"items-center",
 				)}>
-				{!!leftIcon &&<span className='h-5 aspect-square mr-3'>{leftIcon}</span>}
+				{!!leftIcon && <span className='h-5 aspect-square mr-3'>{leftIcon}</span>}
 				<span>{label}</span>
-				{!!rightIcon &&<span className='h-5 aspect-square ml-3'>{rightIcon}</span>}
+				{!!rightIcon && <span className='h-5 aspect-square ml-3'>{rightIcon}</span>}
 			</div>
 		</button>
 
@@ -51,7 +58,6 @@ Button.propTypes = {
 	label: PropTypes.string.isRequired,
 	style: PropTypes.oneOf(["primary", "secondary"]).isRequired,
 	size: PropTypes.oneOf(["small", 'medium', "large"]),
-	onClick: PropTypes.func.isRequired,
 	filled: PropTypes.bool,
 	leftIcon: PropTypes.element,
 	rightIcon: PropTypes.element,
